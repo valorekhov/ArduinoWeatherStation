2c2e69b6f5a5a2b6e329d47f3fda5c73ae61e490
Val Orekhov <val@portalsolutions.net>
2017-08-13 21:10:43 -0400

    Stability fixes

--- WeatherStation/WeatherStation.ino
+++ WeatherStation/WeatherStation.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
 #include "Adafruit_Sensor.h"
-#include "Adafruit_TSL2561_U.h"
+//#include "Adafruit_TSL2561_U.h"
 #include "Adafruit_BMP085_U.h"
 #include "DHT.h"
 #include "Narcoleptic.h"
@@ -14,9 +14,7 @@ DHT dht;
 #define XBEESLEEPPIN 2
 
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
-Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
-
-int sample=0;
+//Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
 
 void setup() {
   pinMode( LEDPIN , OUTPUT);
@@ -26,7 +24,6 @@ void setup() {
 */
 
   Serial.begin(57600); 
-  dht.setup(DHTPIN);
   
   if(!bmp.begin())
   {
@@ -34,18 +31,17 @@ void setup() {
     while(1);
   }
   
-  if(!tsl.begin())
+  /*if(!tsl.begin())
   {
-    /* There was a problem detecting the ADXL345 ... check your connections */
     Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
     while(1);
-  }
+  }*/
   
-  configureLightSensor();
+  //configureLightSensor();
   
   //Delay for 15 sec at startup after a manual RST to allow for the wireless programmer to transition 
   // the board into the bootloader, or change other XBEE settings remotely, c.f. disable pin sleep mode to keep the XBEE up
-  blink(15, 500);
+  //blink(15, 500);
 }
 
 void blink(int times){
@@ -63,38 +59,38 @@ void blink(int times, int delayValue){
 
 void loop() {
   digitalWrite(XBEESLEEPPIN, LOW);  //Wake UP XBEE
-  delay(5000);
+  //delay(2500);
+  dht.setup(DHTPIN);
 
-  blink(10, 50);                    //1 sec delay on wake up to allow the sample to be sent
+
+  blink(30, 50);                    //3 sec delay on wake up to allow the sample to be sent
 
   digitalWrite(LEDPIN, HIGH);  
 
-  Serial.print("EXW: SID="); 
-  Serial.print(sample++); 
-  Serial.print(" "); 
-  
+    
   float bmpTemperature;
   float dhtTemperature = dht.getTemperature();
   bmp.getTemperature(&bmpTemperature);
-  
-  Serial.print("T1="); printValue(bmpTemperature);
-  Serial.print("T2="); printValue(dhtTemperature);
-  Serial.print("RH="); printValue(dht.getHumidity());
-  
+  float humidity = dht.getHumidity();
+
   float tempAvg = NAN;
   if (!isnan(dhtTemperature) && !isnan(bmpTemperature)){
      tempAvg = (dhtTemperature + bmpTemperature) / 2;
   } else if (!isnan(bmpTemperature)){
     tempAvg = bmpTemperature;
   }
-  Serial.print("T="); printValue( tempAvg );
 
-  
   sensors_event_t event;
   bmp.getEvent(&event); 
+
+  Serial.print("EXW: ");
+  Serial.print("T1="); printValue(bmpTemperature);
+  Serial.print("T2="); printValue(dhtTemperature);
+  Serial.print("RH="); printValue(humidity);
+  Serial.print("T="); printValue( tempAvg );  
   Serial.print("P="); printValue(event.pressure);
   
-  uint16_t broadband = 0;
+  /*uint16_t broadband = 0;
   uint16_t infrared = 0;
  
   tsl.getEvent(&event); 
@@ -108,7 +104,7 @@ void loop() {
     Serial.print("AL="); printValue(lx);
     Serial.print("BL="); printValue((long)broadband);
     Serial.print("IR="); printValue((long)infrared);
-  }
+  }*/
   Serial.print("\n");
 
   delay(1500);   //Delay at the end to allow xbee to flush its send buffer 
@@ -148,12 +144,12 @@ void configureLightSensor(void)
   /* You can also manually set the gain or enable auto-gain support */
   // tsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light to avoid sensor saturation */
   // tsl.setGain(TSL2561_GAIN_16X);     /* 16x gain ... use in low light to boost sensitivity */
-  tsl.enableAutoGain(true);             /* Auto-gain ... switches automatically between 1x and 16x */
+  ////tsl.enableAutoGain(true);             /* Auto-gain ... switches automatically between 1x and 16x */
   
   /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
   //tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
   // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
-  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
+  //tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
 }
 
 void sleep(long milliseconds)
